299d3a4d68ee3d4146e3e5bffe9985b7fa3bcdb8
Lynn <1076448574@qq.com>
2018-07-16 14:12:23 +0800

    \1

--- command/esRecord.php
+++ command/esRecord.php
@@ -51,3 +51,78 @@ $str = '{
 		}
 	}
 }';
+
+$esJson = array(
+    "aggs" => array(
+        "date_count"=>array(
+            "nested"=>array(
+                "path"=>"date_total_count",
+            ),
+            "aggs"=>array(
+                "inner"=>array(
+                    "filter"=>array(
+                        "bool"=>array(
+                            "must"=>array(
+                                "range"=>array(
+                                    "date_total_count.created_date"=>array(
+                                        "gte"=>$start_intdate,
+                                        "lte"=>$end_intdate
+                                    )
+                                )
+                            )
+                        )
+                    ),
+                    "aggs"=>array(
+                        "date"=>array(
+                            "terms"=>array(
+                                "field"=>"date_total_count.created_date"
+                            )
+                        )
+                    )
+                )
+
+            )
+        )
+    ),
+    "query" => array(
+        "filtered" => array(
+            "filter" => array(
+                "bool" => array(
+                    "must" => array(
+                        array("term" => array("mid" => $mid)),
+                        array("term" => array("tag_id" => '5127')),
+                        array("nested"=>array(
+                            "path"=>"date_total_count",
+                            "query"=>array(
+                                "bool"=>array(
+                                    "must"=>array(
+                                        "range"=>array(
+                                            "date_total_count.created_date"=>array(
+                                                "gte"=>$start_intdate,
+                                                "lte"=>$end_intdate
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )),
+                        array(
+                            "has_parent"=>array(
+                                'parent_type'=>'wechat_customer',
+                                "query"=>array(
+                                    "bool"=>array(
+                                        "must"=>array(
+                                            array("term" => array("mid" => $mid)),
+                                            array("term" => array("subscribe" => 1)),
+                                            array("term" => array("source_channel_id" => 0)),
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    ),
+                ),
+            )
+        ),
+    )
+);
